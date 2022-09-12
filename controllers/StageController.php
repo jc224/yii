028a28c2b3497ae47c44a8bd9b1fd3a7e1d30711
jc224 <bangalytomata@gmail.com>
2022-09-12 21:53:24 +0200

    sauve

---
+++ controllers/StageController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class StageController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionInsertion(){
+        var_dump(die("salut"));
+    }
+}
---
+++ views/tp/stage.php
@@ -0,0 +1,2 @@
+  <p><a class="btn btn-lg btn-success" type="submit" href="javascript:;" onclick="$('#mainform').submit();">Get started with Yii</a></p>
+
